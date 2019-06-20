550ce8535b85f10332194dae786e3cb9652490e3
Puck Wang <s9801077@gmail.com>
2019-06-21 02:23:14 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ mkdir public
 git worktree prune
 rm -rf .git/worktrees/public/
 
-echo "# Checking out gh-pages branch into public"
+echo "# Checking out master branch into public"
 git worktree add -B master public origin/master
 
 echo "# Removing existing files"
@@ -15,6 +15,6 @@ rm -rf public/*
 echo "# Generating site"
 hugo &&
 
-echo "# Updating gh-pages branch"
-cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
+echo "# Updating master branch"
+cd public && git add --all && git commit -m "Publishing to master (deploy.sh)"
 git push origin master
