de1eea7b3dcda4817ba2e99c116f218b641a213c
Puck Wang <s9801077@gmail.com>
2019-06-21 02:11:24 +0800

    Init commit

---
+++ content/blog/_index.md
@@ -0,0 +1,11 @@
+---
+title: Blog
+
+menu:
+  main:
+    weight: 1
+
+---
+<div style="text-align:center">
+Website coming soon...
+</div>
---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+echo "# Deleting old publication"
+rm -rf public
+mkdir public
+git worktree prune
+rm -rf .git/worktrees/public/
+
+echo "# Checking out gh-pages branch into public"
+git worktree add -B master public origin/master
+
+echo "# Removing existing files"
+rm -rf public/*
+
+echo "# Generating site"
+hugo &&
+
+echo "# Updating gh-pages branch"
+cd public && git add --all && git commit -m "Publishing to gh-pages (deploy.sh)"
+git push origin master
